5c97b9ad35eb3c5fb8215cd8bbec5c53a2f266ee
Jns Hnz <humans@error-undefined.de>
2017-08-06 15:36:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+site2kirby
+========
+
+*Version:* 0.1
+
+This is a python script that loops through a folder with HTM(L) files, batch converting them to [Kirby Markdown](https://getkirby.com/docs/content/text#links) formatted '.txt'-files in a seperate folder using [HTML2Kirby](https://github.com/liip/html2kirby). It keeps the folder structure.
+
+## Authors
+[error:undefined design](http://error-undefined.de/)
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+It is discouraged to use this plugin in any project that promotes racism, sexism, homophobia, animal abuse, violence or any other form of hate speech.
---
+++ site2kirby.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+
+from html2kirby import HTML2Kirby
+
+inputdir = os.getcwd()
+basename = os.path.basename(inputdir)
+outputdir = '../' + basename + '-kirby'
+
+i = 0
+
+# make outputfolder for converted files
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
+
+for root, dirs, filenames in os.walk(inputdir):
+
+    # clone directory structure to outputfolder
+    subfolder = os.path.join(outputdir, root[len(inputdir):])
+    if not os.path.exists(subfolder):
+        os.makedirs(outputdir + subfolder, exist_ok=True)
+
+    # convert and write txt-files to outputfolder structure
+    for filename in [f for f in filenames if (f.endswith('.html')) or (f.endswith('.htm')) ]:
+
+        i += 1
+
+        if root.lstrip(inputdir):
+            subdir = '/' + root.lstrip(inputdir) + '/'
+        else:
+            subdir = '/'
+
+        filewithpath = os.path.join(root, filename)
+
+        file = open(filewithpath, 'r')
+        markup = file.read()
+        file.close()
+
+        # call html2kirby
+        formatter = HTML2Kirby()
+        formatter.feed(markup)
+
+        file_name, file_extension = os.path.splitext(filename)
+        
+        file = open(outputdir + subdir + file_name + '.txt', 'w')
+        file.write(formatter.markdown)
+        file.close()
+
+# remove empty directories from outputfolder
+for root, dirs, filenames in os.walk(outputdir):
+        for dirname in dirs:
+            dir_name = os.path.join(root, dirname)
+            if not os.listdir(dir_name):
+                os.removedirs(dir_name)
+
+print()
+print('==> ', str(i), 'files converted to Kirby Markdown.')
+print('[Converted files were created in: ', outputdir, ']')
+print()
